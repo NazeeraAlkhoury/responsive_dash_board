808760a18deaf690981af87bdbba2eb6a070e351
Nazeera Al Khouri <Nazeeraalkhoury21@gmail.com>
2024-10-24 13:32:01 +0300

    Add fonts

--- lib/view/responsive_dash_board_view.dart
+++ lib/view/responsive_dash_board_view.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dash_board/core/utils/app_assest_image.dart';
+import 'package:responsive_dash_board/core/utils/app_colors.dart';
 
 class ResponsiveDashBoardView extends StatelessWidget {
   const ResponsiveDashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children:[
+        Card(
+          color: AppColors.greyBackgroundColor,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: ListTile(
+          leading: SvgPicture.asset(AppImageAssets.avatar_3),
+          title: Text('Lekan Okeowo'),
+          subtitle: Text('demo@gmail.com'),
+        ),
+        ),
+        
+      ],
+    );
   }
 }
