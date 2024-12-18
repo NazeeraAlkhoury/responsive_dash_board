273ba36544accf6d456f5777812c3cb585faaabb
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-18 14:39:20 +0300

    Create CustomDropDown widget

---
+++ lib/widgets/custom_dropdown.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utils/app_colors.dart';
+import 'package:responsive_dash_board/core/utils/app_text_styles.dart';
+
+class CustomDropDown extends StatelessWidget {
+  const CustomDropDown({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: AppColors.borderColor,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          children: [
+            const Text(
+              'Monthly',
+              style: AppTextStyles.styleMedium16,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Transform.rotate(
+              angle: -1.5708,
+              child: const Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: AppColors.mainTextColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
