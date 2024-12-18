a272a2b6ab10500297cc045c9a6a2046793ea72a
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-18 14:05:17 +0300

    Update CustomDrawer with background color

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/core/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/core/models/user_info_model.dart';
 import 'package:responsive_dash_board/core/utils/app_assest_image.dart';
+import 'package:responsive_dash_board/core/utils/app_colors.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer_item.dart';
 import 'package:responsive_dash_board/widgets/custom_user_info_list_tile.dart';
 import 'package:responsive_dash_board/widgets/drawer_list.dart';
@@ -13,49 +14,55 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 15,
-              ),
-              CustomUserInfoListTile(
-                userInfoModel: UserInfoModel(
-                  image: AppImageAssets.avatar_3,
-                  name: 'Lekan Okeowo',
-                  email: 'demo@gmail.com',
+          child: Container(
+            color: AppColors.containerBackgroundColor,
+            child: const Column(
+              children: [
+                SizedBox(
+                  height: 15,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              DrawerList(),
-              Expanded(
-                child: SizedBox(
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12),
+                  child: CustomUserInfoListTile(
+                    userInfoModel: UserInfoModel(
+                      image: AppImageAssets.avatar_3,
+                      name: 'Lekan Okeowo',
+                      email: 'demo@gmail.com',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                DrawerList(),
+                Expanded(
+                  child: SizedBox(
+                    height: 30,
+                  ),
+                ),
+                CustomDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                    image: AppImageAssets.settings,
+                    title: 'Setting system',
+                  ),
+                  isActive: false,
+                ),
+                CustomDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                    image: AppImageAssets.logout,
+                    title: 'Logout account',
+                  ),
+                  isActive: false,
+                ),
+                SizedBox(
                   height: 30,
                 ),
-              ),
-              CustomDrawerItem(
-                drawerItemModel: DrawerItemModel(
-                  image: AppImageAssets.settings,
-                  title: 'Setting system',
-                ),
-                isActive: false,
-              ),
-              CustomDrawerItem(
-                drawerItemModel: DrawerItemModel(
-                  image: AppImageAssets.logout,
-                  title: 'Logout account',
-                ),
-                isActive: false,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
