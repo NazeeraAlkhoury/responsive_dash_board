a96165e585ee558e99d75f744cad2e3e17397915
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-18 14:43:03 +0300

    Create CustomAllExpensessHeader widget

---
+++ lib/widgets/custom_all_expensess_header.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utils/app_text_styles.dart';
+import 'package:responsive_dash_board/widgets/custom_dropdown.dart';
+
+class CustomAllExpensesHearder extends StatelessWidget {
+  const CustomAllExpensesHearder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'All Expenses',
+          style: AppTextStyles.styleSemiBold20,
+        ),
+        CustomDropDown(),
+      ],
+    );
+  }
+}
