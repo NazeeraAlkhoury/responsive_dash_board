a3c4d322e954fe30f357d355320582a605289bcf
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-19 14:10:13 +0300

    Add AllExpensesItemModel to represent expense items

---
+++ lib/core/models/expensess_item_model.dart
@@ -0,0 +1,9 @@
+class ExpensessItemModel {
+  final String image, title, date, amount;
+
+  const ExpensessItemModel(
+      {required this.image,
+      required this.title,
+      required this.date,
+      required this.amount});
+}
