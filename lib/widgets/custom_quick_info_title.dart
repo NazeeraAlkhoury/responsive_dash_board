9fbb6314f31add90cec202586fd663abbb313d1c
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-19 20:50:06 +0300

    Create CustomQuicInoviceTitle widget

---
+++ lib/widgets/custom_quick_info_title.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utils/app_text_styles.dart';
+
+class CustomQuicInoviceTitle extends StatelessWidget {
+  final String title;
+  const CustomQuicInoviceTitle({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: AppTextStyles.styleMedium16,
+    );
+  }
+}
