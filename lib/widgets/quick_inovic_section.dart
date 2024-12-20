586d52104c2f2beba2c0fec29ab925c44250d0a3
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-20 12:11:06 +0300

    Create QuickInovicSection

---
+++ lib/widgets/quick_inovic_section.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utils/app_colors.dart';
+import 'package:responsive_dash_board/widgets/custom_container.dart';
+import 'package:responsive_dash_board/widgets/custom_latest_transaction_list.dart';
+import 'package:responsive_dash_board/widgets/custom_quick_info_title.dart';
+import 'package:responsive_dash_board/widgets/form_section.dart';
+import 'package:responsive_dash_board/widgets/quick_inovice_header.dart';
+
+class QuickInoviceSection extends StatelessWidget {
+  const QuickInoviceSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomContainer(
+      child: (BuildContext context) => const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          QuickInvoiceHeader(),
+          CustomQuicInoviceTitle(
+            title: 'Latest Transaction',
+          ),
+          CustomLatestTransactionList(),
+          Divider(
+            color: AppColors.borderColor,
+            height: 42,
+          ),
+          FormSection(),
+        ],
+      ),
+    );
+  }
+}
