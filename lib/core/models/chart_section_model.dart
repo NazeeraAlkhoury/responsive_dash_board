f287bc9dc40526232f184bb1cc714fec9b925552
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-21 15:38:21 +0300

    Create ChartSecionModel to represent pie chart data

---
+++ lib/core/models/chart_section_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ChartSectionModel {
+  final Color color;
+  final double value;
+
+  const ChartSectionModel({required this.color, required this.value});
+}
