278642223999be6c088190e711eab65e0137c34e
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-21 19:04:20 +0300

    Create CustomDetailsItem widget

---
+++ lib/widgets/custom_details_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/models/chart_section_model.dart';
+import 'package:responsive_dash_board/core/utils/app_text_styles.dart';
+
+class CustomDetailsItem extends StatelessWidget {
+  final ChartSectionModel chartSectionModel;
+  const CustomDetailsItem({
+    super.key,
+    required this.chartSectionModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.all(0),
+      leading: CircleAvatar(
+        radius: 6,
+        backgroundColor: chartSectionModel.color,
+      ),
+      title: FittedBox(
+        fit: BoxFit.scaleDown,
+        alignment: AlignmentDirectional.centerStart,
+        child: Text(
+          chartSectionModel.title,
+          style: AppTextStyles.styleRegular16,
+        ),
+      ),
+      trailing: Text(
+        chartSectionModel.value.toString(),
+        style: AppTextStyles.styleMedium16,
+      ),
+    );
+  }
+}
