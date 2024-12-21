a416bd6c60d3df48faf3f305ca6c5b2759d3ad35
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-21 16:40:27 +0300

    Create piedetailsData

---
+++ lib/core/data/static/pie_details_data.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+import 'package:responsive_dash_board/core/models/chart_section_model.dart';
+
+List<ChartSectionModel> pieChartSectionData = const [
+  ChartSectionModel(
+    color: Color(0xFF208CC8),
+    value: 40,
+    title: 'Design service',
+  ),
+  ChartSectionModel(
+    color: Color(0xFF4EB7F2),
+    value: 25,
+    title: 'Design product',
+  ),
+  ChartSectionModel(
+    color: Color(0xFF064061),
+    value: 20,
+    title: 'Product royalti',
+  ),
+  ChartSectionModel(
+    color: Color(0xFFE2DECD),
+    value: 22,
+    title: 'Other',
+  ),
+];
