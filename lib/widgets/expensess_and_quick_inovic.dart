6f985445db9191f4a5f39c08afc91cfe1172e5f1
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-21 17:56:26 +0300

    Create ExpensessAndQuickInovic

---
+++ lib/widgets/expensess_and_quick_inovic.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_all_expensess_section.dart';
+import 'package:responsive_dash_board/widgets/quick_inovic_section.dart';
+
+class ExpensessAndQuickInovic extends StatelessWidget {
+  const ExpensessAndQuickInovic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAllExpensessSection(),
+        SizedBox(
+          height: 10,
+        ),
+        QuickInoviceSection(),
+      ],
+    );
+  }
+}
